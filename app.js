3c5b762bfe000f0b3486e9dd2a292d30b526722b
nohid17 <nohid.ahmed@gmail.com>
2026-02-22 15:07:15 -0500

    DOM link

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-console.dir(document);
+// console.dir(document);
 
 // // alert();
 // // window.alert();
 
 // window.document
 // document
+
+document.body.children[1].children[0].href = 'https://google.com';
